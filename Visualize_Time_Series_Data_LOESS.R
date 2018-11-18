76046bd7bf7b4d143ba055998d0bc00a1363f340
Benjamin Hardisty <ben.hardisty1@gmail.com>
2018-11-18 15:14:20 -0700

    Add files via upload

--- Visualize_Time_Series_Data_LOESS.R
+++ Visualize_Time_Series_Data_LOESS.R
@@ -1,10 +1,9 @@
-## Lab March 8
+## March 8 2011
 ## Using Loess Curves to Visualize Time Series
 
 i <- 1 + seq(0, lengthco2-1)%%12
 
 mo <- month.abb[1]
-
 mo <- ordered(mo, levels=month.abb)
 
 resdat <- (res~t1mo, data=resdat) + layer(panel.loess(x, y, degree=2, span=2/3, col="red"))
